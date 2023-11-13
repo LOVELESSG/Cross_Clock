fecb450148762711a260e823e362b30e893aadeb
LOVELESSG <koreabnl@gmail.com>
2023-11-13 19:57:39 +0900

    start building the database structure for alarm

--- app/src/main/java/com/example/crossclock/data/alarm/Alarm.kt
+++ app/src/main/java/com/example/crossclock/data/alarm/Alarm.kt
@@ -1,10 +1,13 @@
 package com.example.crossclock.data.alarm
 
+import androidx.room.Entity
 import java.time.LocalTime
+import java.time.ZoneId
 
+@Entity
 data class Alarm(
     val time: LocalTime,
     val message: String,
-    val timeZone: String,
+    val timeZone: ZoneId,
     val date: String
 )
---
+++ app/src/main/java/com/example/crossclock/data/alarm/AlarmDao.kt
@@ -0,0 +1,4 @@
+package com.example.crossclock.data.alarm
+
+interface AlarmDao {
+}
