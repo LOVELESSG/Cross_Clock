fcc68fe3e4e39f13fe956d5d367bab9a1846327e
LOVELESSG <koreabnl@gmail.com>
2023-12-02 01:47:28 +0900

    alarm can send notification now

--- app/src/main/java/com/example/crossclock/ui/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/crossclock/ui/alarm/AlarmReceiver.kt
@@ -1,30 +1,55 @@
 package com.example.crossclock.ui.alarm
 
+import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.crossclock.MainActivity
+import com.example.crossclock.R
 
 class AlarmReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent?) {
         val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
         val ringtone = RingtoneManager.getRingtone(context, alarmSound)
         ringtone.play()
 
-        val notification = context?.let {
-            NotificationCompat.Builder(it, "Alarm_Channel")
-                .setContentTitle("Alarm is on")
-                .setContentText("Wake up")
-                .setSmallIcon(1)
-                .build()
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "alarm_channel"
+            val channelName = "channel for alarm"
+            val channel = NotificationChannel(
+                channelId, channelName, NotificationManager.IMPORTANCE_HIGH).apply {
+                    lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+            }
+            notificationManager.createNotificationChannel(channel)
         }
 
-        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(123, notification)
+        val fullScreenIntent = Intent(context, MainActivity::class.java)
+        val fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
+            fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification = NotificationCompat.Builder(context, "alarm_channel")
+            .setContentTitle("Alarm is on")
+            .setContentText("Wake up")
+            .setSmallIcon(R.drawable.baseline_alarm_on_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .setAutoCancel(true)
+            .setFullScreenIntent(fullScreenPendingIntent, true)
+
+        val alarmNotification = notification.build()
+        notificationManager.notify(24778, alarmNotification)
     }
 }
