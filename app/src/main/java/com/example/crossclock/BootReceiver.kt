a38d3ac87b74b400463d0e2e9f5bbd5bbaeee960
LOVELESSG <koreabnl@gmail.com>
2023-12-09 01:01:28 +0900

    use coroutine to replace AsyncTask

--- app/src/main/java/com/example/crossclock/BootReceiver.kt
+++ app/src/main/java/com/example/crossclock/BootReceiver.kt
@@ -5,25 +5,13 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.AsyncTask
-import android.os.Looper
-import android.util.Log
 import com.example.crossclock.data.AppDatabase
-import com.example.crossclock.data.Repository
-import com.example.crossclock.data.alarm.Alarm
 import com.example.crossclock.ui.alarm.AlarmReceiver
-import com.example.crossclock.ui.alarm.CrossAlarmScheduler
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.util.logging.Handler
 
 class BootReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -32,39 +20,35 @@ class BootReceiver: BroadcastReceiver() {
         }
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     private fun reSetAllAlarms(context: Context){
-        val task = object : AsyncTask<Void, Void, Void>() {
-            override fun doInBackground(vararg params: Void?): Void? {
-                val db = AppDatabase.getDatabase(context)
-                val alarms = db.alarmDao().loadAllAlarmInList()
+        GlobalScope.launch {
+            val db = AppDatabase.getDatabase(context)
+            val alarms = db.alarmDao().loadAllAlarmInList()
 
-                for (alarm in alarms) {
-                    val alarmTime = alarm.time.atZone(alarm.timeZone)
-                    val nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
-                    val compareTime = nowTime.isBefore(alarmTime)
-                    if (alarm.onOrOff && compareTime) {
-                        val alarmManager =
-                            context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                        val alarmIntent = Intent(context, AlarmReceiver::class.java)
-                        val pendingIntent = PendingIntent.getBroadcast(
-                            context,
-                            alarm.id,
-                            alarmIntent,
-                            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-                        )
+            for (alarm in alarms) {
+                val alarmTime = alarm.time.atZone(alarm.timeZone)
+                val nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
+                val compareTime = nowTime.isBefore(alarmTime)
+                if (alarm.onOrOff && compareTime) {
+                    val alarmManager =
+                        context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                    val alarmIntent = Intent(context, AlarmReceiver::class.java)
+                    val pendingIntent = PendingIntent.getBroadcast(
+                        context,
+                        alarm.id,
+                        alarmIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    )
 
-                        alarmManager.setExactAndAllowWhileIdle(
-                            AlarmManager.RTC_WAKEUP,
-                            alarm.time.atZone(alarm.timeZone).toEpochSecond() * 1000,
-                            pendingIntent
-                        )
-                    }
+                    alarmManager.setExactAndAllowWhileIdle(
+                        AlarmManager.RTC_WAKEUP,
+                        alarm.time.atZone(alarm.timeZone).toEpochSecond() * 1000,
+                        pendingIntent
+                    )
                 }
-                return null
             }
         }
-
-        task.execute()
     }
 }
 
