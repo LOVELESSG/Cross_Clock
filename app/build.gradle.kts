2d9e14d4ec3d707946070d34e6a1c476987c511c
Li Longzhen <koreabnl@gmail.com>
2026-04-17 23:24:39 +0900

    update targetSdk version to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.crossware.crossclock"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.crossware.crossclock"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 5
         versionName = "1.4"
 
